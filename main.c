5686b9d0511e35c4ab846e4487a26dc8f1ce94c1
DaFluffyOwl <creeper47@outlook.com>
2022-07-17 18:04:27 -0400

    Added main square

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+const int WIDTH = 20;
+const int HEIGHT = 10;
 int main(){
-    printf("Hello World");
+    printf("                                          ");
+    for(int i = 0; i < HEIGHT; i++){
+        for(int j = 0; j < WIDTH; j++){
+            printf("#");
+        }
+        printf("\n");
+        printf("                                          ");
+    }
+    printf("\n");
 }
